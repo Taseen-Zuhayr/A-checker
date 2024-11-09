36db9d68a6d64f017654f860b9e718993d89da80
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-11-09 21:34:28 +0600

    A checker

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ word = input("Enter Word : ")
 
 for i in word:
     if i=="A" or i=="a":
-        print("We found a.")
+        print("We found the a. I am taking it to the gulag.")
         break
     else:
-        print("We lost the a.")
+        print("We lost the a. we,ll gat it next time.")
